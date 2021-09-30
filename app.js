b3188b38134f43671de820e692930c7d21eea380
mira-kine <mirak0419@gmail.com>
2021-09-30 14:17:06 -0700

    Created UI for add

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { add } from './calculations.js'
+console.log(add);
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const addButton = document.getElementById('add-button');
@@ -5,3 +7,10 @@ const addAnswer = document.getElementById('add-answer');
 
 
 console.log(addInput1, addInput2, addButton. addAnswer)
+
+addButton.addEventListener('click', ()=>{
+  const value1 = Number(addInput1.value);
+  const value2 = Number(addInput2.value);
+  const result = add(value1, value2);
+  console.log(result);
+});
