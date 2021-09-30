809e12f369a5cfd32bbe206d4167b1360b2f33ce
mira-kine <mirak0419@gmail.com>
2021-09-30 15:09:59 -0700

    created failing and passing test, ultimately passed

--- calculations.js
+++ calculations.js
@@ -5,3 +5,11 @@ export function add_two(num){
 export function add(num1, num2) {
    return num1 + num2;
 }
+
+export function subtract_two(num){
+    return num - 2;
+}
+
+export function subtract(num1, num2) {
+    return num1 - num2;
+}
--- test/example.test.js
+++ test/example.test.js
@@ -2,6 +2,7 @@
 // import { example } from '../example.js';
 
 import { add_two, add } from '../calculations.js';
+import { subtract_two, subtract } from '../calculations.js';
 
 const test = QUnit.test;
 
@@ -33,4 +34,18 @@ test('add(4,5) function', (expect) => {
     const expected = 9;
     const actual = add(input1, input2);
     expect.equal(actual, expected);
+});
+
+test ('test subtract_two function', (expect) => {
+    const expected = 2;
+    const actual = subtract_two(4);
+    expect.equal(actual, expected);
+})
+
+test ('subtract(10,5) function', (expect) => {
+    const input1 = 10;
+    const input2 = 5;
+    const expected = 5;
+    const actual = subtract(input1, input2);
+    expect.equal(actual,expected);
 });
