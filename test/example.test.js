2ac14ff16d60cf275a343dc8d24bebe6de2a7825
mira-kine <mirak0419@gmail.com>
2021-09-30 13:47:13 -0700

    created two tests that check for addition function

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,36 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { add_two, add } from '../calculations.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('test add_two function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 4;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add_two(2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('add (2,3) function', (expect) => {
+    const input1 = 2;
+    const input2 = 3;
+    const expected = 5;
+    const actual = add(input1, input2);
+    expect.equal(actual, expected);
+});
+
+test('add(4,5) function', (expect) => {
+    const input1 = 4;
+    const input2 = 5;
+    const expected = 9;
+    const actual = add(input1, input2);
     expect.equal(actual, expected);
 });
