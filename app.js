fd15aff28ff1c323dfd9d86f19cdb34feb256e41
mira-kine <mirak0419@gmail.com>
2021-09-30 14:09:11 -0700

    add function, app.js addition and styled

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-// import functions and grab DOM elements
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
 
-// initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+console.log(addInput1, addInput2, addButton. addAnswer)
